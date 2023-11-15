f40dd475cdf873a212f0ba3e96c84aa122888127
Alan Andre <114703935+AlanAndreUP@users.noreply.github.com>
2023-11-15 08:07:01 -0600

    06 UPDATE CORS DEVALAN

--- scr/app.js
+++ scr/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 require('dotenv').config();
