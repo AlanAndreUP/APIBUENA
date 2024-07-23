82980386325209e63310e7bde0c97669972f0f17
AlanAndreUp <cosaspami7@gmail.com>
2024-07-23 17:42:00 -0600

    wip:

--- scr/app.ts
+++ scr/app.ts
@@ -29,12 +29,14 @@ db.once('open', () => {
   console.log('Conexión exitosa a la base de datos MongoDB.');
 });
 
+// Configuración de CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/pasajeros', pasajerosRoutes);  
