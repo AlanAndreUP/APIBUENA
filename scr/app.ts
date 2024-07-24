83acc7a269cdede9540dcc4a015d924fb056bab7
AlanAndreUp <cosaspami7@gmail.com>
2024-07-23 18:06:17 -0600

    2

--- scr/app.ts
+++ scr/app.ts
@@ -34,6 +34,9 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
   next();
 });
 
