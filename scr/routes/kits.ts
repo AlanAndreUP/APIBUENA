dbc6ef4457e59dd16a9fedf20a10c3c85dbf0e67
Herobroto <herobrotelpro@gmail.com>
2024-07-30 07:02:51 -0600

    ganancias por hora y endpoint para popularizar una semana previa

--- scr/routes/kits.ts
+++ scr/routes/kits.ts
@@ -59,10 +59,11 @@ router.get("/:_idKit/gps/historial", authenticateToken, async (req: Request, res
 router.get('/gps/historial', authenticateToken, async (req: Request, res: Response) => {
     try {
         const fechaUsuario = new Date();
-        const fechaUnaHoraAntes = new Date(fechaUsuario.getTime() - (24 * 60 * 60 * 1000)); // 1 día antes
+        let falseUsuario = new Date(fechaUsuario.getTime() - (2 * 60 * 60 * 1000))
+        const fechaUnaHoraAntes = new Date(falseUsuario.getTime() - (24 * 60 * 60 * 1000)); // 1 día antes
 
         let kits = await Kit.find({
-            "historial.fecha": { $gte: fechaUnaHoraAntes, $lte: fechaUsuario }
+            "historial.fecha": { $gte: fechaUnaHoraAntes, $lte: falseUsuario }
         });
 
         if (!kits || kits.length === 0) return res.status(400).json({ message: 'No se encontraron kits' });
