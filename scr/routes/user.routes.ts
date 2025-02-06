a9845a99e50a5d61032cdd246ffd761588c02794
Herobroto <herobrotelpro@gmail.com>
2025-02-06 00:36:57 -0600

    add route image

--- scr/routes/user.routes.ts
+++ scr/routes/user.routes.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
+router.post('/image', userController.loginByCamera);
 router.get('/info/:_id', userController.getUserById);
 router.put('/update/:_id', userController.updateUser);
 router.delete('/delete/:_id', userController.deleteUser);
